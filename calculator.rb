e702cca4c4f53a96454da8242f7f63ec4795578a
Erica <ericabasak@gmail.com>
2019-09-06 15:28:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,3 +9,17 @@ def sum1(num)
   sum = first_number + second_number
   result sum
 end
+
+
+def difference()
+  diff = first_number - second_number
+  result diff
+end
+
+
+def product()
+  product = first_number * second_number
+  result product
+end
+
+def 
