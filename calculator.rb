e0a289f8d456aa6f51dfe301e465a57aff25df4f
Erica <ericabasak@gmail.com>
2019-09-06 15:35:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,22 +7,11 @@ second_number = 2
 
 sum = first_number + second_number
 
+difference = first_number - second_number
 
+product = first_number * second_number
 
-def difference()
-  difference = first_number - second_number
-  result difference
-end
+quotient = first_number / second_number
 
-
-def product()
-  product = first_number * second_number
-  result product
-end
-
-def quotient()
-  quotient = first_number / second_number
-  result quotient
-end
 
 
